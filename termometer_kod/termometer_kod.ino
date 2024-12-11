f15edc87017ee5a6199cf4ca086fe28fd718148d
Rasmus <rasmus.liljeberg@outlook.com>
2024-12-11 13:08:07 +0100

    started on version 2 and included libraries with sketch

--- termometer_kod/termometer_kod.ino
+++ termometer_kod/termometer_kod.ino
@@ -1,20 +1,19 @@
-#include <SmoothThermistor.h>
-#include <ChuckPlex.h>
+#include "src/ChuckPlex-19a3b/ChuckPlex.h"
+#include "src/SmoothThermistor/src/SmoothThermistor.h"
 #include <Preferences.h>
 #include "charlie.h"
 
 #define WAKEUP_GPIO GPIO_NUM_3
 
-int mode;
+int val;
 int temp;
-int lasttemp;
 int ext;
 const int cykel = 500;
 const int periodvaken = 3000;
 const int periodvaken_inst = 10000;
 const int termistorcykel = 10000;
-const int cal = 28; //prot 32
-const int thermcal = 33;
+const int kal = 30; //prot 32
+const int termkal = 35;
 unsigned long mil;
 unsigned long milvakna;
 
@@ -28,7 +27,7 @@ SmoothThermistor externtemp(1, ADC_SIZE_12_BIT, 10000, 10000, 3950, 25, 3);
 
 void setup() {
   minne.begin("mode", false);
-  mode = minne.getInt("mode", 0); //ta värde från minne
+  val = minne.getInt("mode", 0); //ta värde från minne
   //mode = 0;
 
   pinMode(3, INPUT);
@@ -37,8 +36,8 @@ void setup() {
 
   esp_deep_sleep_enable_gpio_wakeup(1 << 3, ESP_GPIO_WAKEUP_GPIO_LOW); //förbered för sovläge som avaktivaras av 3 = 0
 
-  ext = externtemp.temperature() + thermcal;
-  temp = map(analogRead(0), 123, 1241, -40, 50) + cal; //kolla temp direkt
+  ext = externtemp.temperature() + termkal;
+  temp = map(analogRead(0), 123, 1241, -40, 50) + kal; //kolla temp direkt
 
   //Serial.begin(115200);
   //while(!Serial){}
@@ -56,12 +55,12 @@ void setup() {
 void loop() {
 
   //lägen
-  if(mode == 0){inut();} //0C
-  if(mode == 1){utin();} //10C
-  if(mode == 2){insov();} //20C
-  if(mode == 3){utsov();}//30C
-  if(mode == 4){in();} //40C
-  if(mode == 5){ut();} //50C
+  if(val == 0){inut();} //0C
+  if(val == 1){utin();} //10C
+  if(val == 2){insov();} //20C
+  if(val == 3){utsov();}//30C
+  if(val == 4){in();} //40C
+  if(val == 5){ut();} //50C
 }
 
 void config(){ //inställningar
@@ -70,19 +69,19 @@ void config(){ //inställningar
     if(!digitalRead(3)){
       while(!digitalRead(3)){}
       delay(100);
-      mode++;
-      if(mode == 6){mode = 0;}
+      val++;
+      if(val == 6){val = 0;}
       mil = millis(); //återställ tid när knapp trycks
     }
 
-    if(mode == 0){visa(39);}
-    if(mode == 1){visa(49);}
-    if(mode == 2){visa(59);}
-    if(mode == 3){visa(69);}
-    if(mode == 4){visa(79);}
-    if(mode == 5){visa(88);}
+    if(val == 0){visa(39);}
+    if(val == 1){visa(49);}
+    if(val == 2){visa(59);}
+    if(val == 3){visa(69);}
+    if(val == 4){visa(79);}
+    if(val == 5){visa(88);}
   }
-  minne.putInt("mode", mode);
+  minne.putInt("mode", val);
 }
 
 void inut(){ //visar innetemperaturen innan utetemp
@@ -92,7 +91,7 @@ void inut(){ //visar innetemperaturen innan utetemp
   }
   if(analogRead(1) > 10){
     mil = millis();
-    ext = externtemp.temperature() + thermcal;
+    ext = externtemp.temperature() + termkal;
   } //kolla temp ibland
     while(millis() < mil + periodvaken){
       visa(ext);
@@ -119,7 +118,7 @@ void inalltid(){ //alltid igång, inne
   if(millis() - mil > cykel){
     mil = millis();
 
-    temp = map(analogRead(0), 123, 1241, -40, 50) + cal;
+    temp = map(analogRead(0), 123, 1241, -40, 50) + kal;
     } //kolla temp ibland
     
   
@@ -133,7 +132,7 @@ void utalltid(){ //alltid igång, ute
     if(millis() - mil > termistorcykel){
       mil = millis();
       plexy.clear();
-      ext = externtemp.temperature() + thermcal;
+      ext = externtemp.temperature() + termkal;
     } //kolla temp ibland
 
     }
@@ -154,7 +153,7 @@ void insov(){ //på/av med knappen
   if(millis() - mil > cykel){
     mil = millis();
 
-    temp = map(analogRead(0), 123, 1241, -40, 50) + cal;
+    temp = map(analogRead(0), 123, 1241, -40, 50) + kal;
   }
 
   visa(temp);
@@ -164,6 +163,7 @@ void insov(){ //på/av med knappen
     delay(100);
     esp_deep_sleep_start();
   }
+  plexy.clear();
 }
 
 void utsov(){ //på/av med knappen
@@ -171,7 +171,7 @@ void utsov(){ //på/av med knappen
   if(millis() - mil > termistorcykel){
     mil = millis();
 
-    ext = externtemp.temperature() + thermcal;
+    ext = externtemp.temperature() + termkal;
   }
 
   visa(ext);
@@ -181,6 +181,7 @@ void utsov(){ //på/av med knappen
     delay(100);
     esp_deep_sleep_start();
   }
+  plexy.clear();
 }
 
 void in(){
@@ -194,7 +195,7 @@ void in(){
 
 void ut(){
   if(analogRead(1) > 10){
-    ext = externtemp.temperature() + thermcal; //kolla temp
+    ext = externtemp.temperature() + termkal; //kolla temp
     while(millis() < mil + periodvaken){
       visa(ext);
     }
